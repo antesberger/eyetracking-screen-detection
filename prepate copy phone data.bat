f005f2715e01baea9be5e51eac14e86886c1041c
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2019-01-29 14:30:16 +0100

    removed old files

--- prepate copy phone data.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-adb shell run-as gazetracking.lmu.com.eyetracking_map_webview "rm -r .Fabric;"
-adb shell run-as com.google.firebase.codelab.friendlychat "rm -r .Fabric;"
-adb shell run-as com.lmu.gazetracking.eyetrackinggallery "rm -r .Fabric;"
-adb shell run-as gazetracking.lmu.com.eyetacking_accuracy_testing "rm -r .Fabric;"

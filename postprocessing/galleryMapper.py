8842d31923d604f3c587e0fb6426871dad0c6a9b
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2019-01-29 15:22:33 +0100

    adding postprocessing tools

---
+++ postprocessing/galleryMapper.py
@@ -0,0 +1,125 @@
+import cv2
+import json
+import numpy as np
+import time
+import csv
+import os
+import sys
+from datetime import datetime
+
+if len(sys.argv) != 2:
+    print "Please provide the folder name as an argument (e.g. "
+else:
+    data = sys.argv[1]
+
+images = ['activity_1','activity_2','activity_3','activity_4','activity_5','activity_6','activity_7','activity_8','activity_9','friends_1','friends_2','friends_3','friends_4','friends_5','friends_6','friends_7','friends_8','friends_9','friends_10','selfie_1','selfie_2','selfie_3','selfie_4','selfie_5','selfie_6','selfie_7','selfie_8','selfie_9','selfie_10','captioned_1','captioned_2','captioned_3','captioned_4','captioned_5','captioned_6','captioned_7','captioned_8','captioned_9','captioned_10','gadget_1','gadget_2','gadget_3','gadget_4','gadget_5','gadget_6','gadget_7','gadget_8','gadget_9','gadget_10','pet_1','pet_2','pet_3','pet_4','pet_5','pet_6','pet_7','pet_8','pet_9','pet_10','fashion_1','fashion_2','fashion_3','fashion_4','fashion_5','fashion_6','fashion_7','fashion_8','fashion_9','fashion_10','food_1','food_2','food_3','food_4','food_5','food_6','food_7','food_8','food_9','food_10']
+imagesHeight = [480, 480, 480, 405, 500, 405, 480, 925, 480, 450, 540, 510, 480, 530, 480, 480, 480, 480, 510, 480, 480, 480, 480, 480, 468, 960, 480, 480, 1080, 960, 560, 480, 480, 480, 540, 405, 540, 480, 940, 480, 480, 480, 450, 480, 510, 480, 480, 480, 405, 480, 480, 480, 1083, 406, 540, 480, 480, 540, 1080, 480, 480, 480, 480, 480, 430, 440, 480, 390, 480, 480, 480, 480, 480, 840, 480, 480, 405, 420, 430]
+imagesHeight = [(i * 2) + 55 for i in imagesHeight]
+
+imageData = open('./data/phone/gallery/' + data + '/scrollInfo.txt', 'r')
+randomOrder = open('./data/phone/gallery/' + data + '/imageorder.txt', 'r')
+scrollLine = imageData.readline()
+nextScrollLine = imageData.readline()
+
+randomOrderIndices = randomOrder.readline()
+randomOrderIndices = randomOrder.readline()
+randomOrderIndices = randomOrderIndices[:-1].split(";  ")[1].split(", ")
+
+#restore random order
+i = len(imagesHeight) - 1
+for index in randomOrderIndices:
+    tmp = images[int(index)]
+    images[int(index)] = images[i]
+    images[i] = tmp
+
+    tmp = imagesHeight[int(index)]
+    imagesHeight[int(index)] = imagesHeight[i]
+    imagesHeight[i] = tmp
+
+    i = i - 1
+
+with open('./out/' + data + '/eyetracking.csv', mode='r') as csvinput:
+    csv_reader = csv.reader(csvinput, delimiter=',')
+
+    all = []
+
+    header = True
+    for row in csv_reader:
+        if header == True:
+            header = False
+            continue
+
+        gazeTs = row[0]
+        gazeX = int(float(row[1]))
+        gazeY = int(float(row[2]))
+
+        try: #milliseconds are missing for a few eyetracking timestamps
+            gazeTs = datetime.strptime(gazeTs, "%Y-%m-%d %H:%M:%S.%f")
+        except:
+            gazeTs = datetime.strptime(gazeTs, "%Y-%m-%d %H:%M:%S")
+
+        if nextScrollLine.split("; ")[0] == '':
+            continue
+
+        scrollTs = datetime.strptime(scrollLine.split("; ")[0], "%Y-%m-%d %H:%M:%S.%f")
+        nextScrollTs = datetime.strptime(nextScrollLine.split("; ")[0], "%Y-%m-%d %H:%M:%S.%f")
+
+        #if scrolled in meantime
+        if gazeTs >= nextScrollTs:
+            scrollLine = nextScrollLine
+            nextScrollLine = imageData.readline()
+            scrollLineSplit = scrollLine.split("; ")
+
+        try:
+            scrollLineSplit[2]
+        except:
+            print "except"
+            continue
+
+        #print(imageDataLine[2])
+        lastVisibleImage = int(scrollLineSplit[1])
+        relativeScrollPostion = scrollLineSplit[2].split(",")
+
+        if len(relativeScrollPostion) == 3:
+            relativeScrollPostion = relativeScrollPostion[2][:-2]
+            offset = 0
+            i = 0
+
+            while i < lastVisibleImage:
+                offset += int(imagesHeight[i])
+                i += 1
+
+            try:
+                firstImage = int(relativeScrollPostion)
+                secondImage = firstImage + imagesHeight[i+1]
+                thirdImage = secondImage + imagesHeight[i+2]
+            except:
+                pass
+
+            if gazeX > 0 and gazeX < 1440:
+                if gazeY > 0 and gazeY <= firstImage:
+                    row.append(images[i])
+                    all.append(row)
+                    print(str(gazeTs) + ": " + images[i])
+                elif gazeY <= secondImage:
+                    row.append(images[i+1])
+                    all.append(row)
+                    print(str(gazeTs) + ": " + images[i+1])
+                elif gazeY < 2888 and gazeY <= thirdImage:
+                    row.append(images[i+2])
+                    all.append(row)
+                    print(str(gazeTs) + ": " + images[i+2])
+                else:
+                    row.append('none')
+                    all.append(row)
+                    print(str(gazeTs) + ": " + 'none')
+            else:
+                row.append('none')
+                all.append(row)
+                print(str(gazeTs) + ": " + 'none')
+        else:
+            print "problems reading"
+
+    with open('./out/' + data + '/eyetrackinggall.csv', mode='a') as csvoutput:
+        csv_writer = csv.writer(csvoutput, lineterminator='\n')
+        csv_writer.writerows(all)
---
+++ postprocessing/gazePointVideo.py
@@ -0,0 +1,117 @@
+import cv2
+import json
+import numpy as np
+import time
+import csv
+import os
+import sys
+from datetime import datetime
+
+dist = np.array([[0.05357947, -0.22872005, -0.00118557, -0.00126952, 0.2067489 ]])
+cameraMatrix = np.array([[1.12585498e+03, 0.00000000e+00, 9.34478069e+02], [0.00000000e+00, 1.10135217e+03, 5.84380561e+02], [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+mtx = np.array([[1.12825274e+03, 0.00000000e+00, 9.35684715e+02], [0.00000000e+00, 1.10801151e+03, 5.86151765e+02], [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+
+if len(sys.argv) != 2:
+    print "Please provide the directory name as an argument"
+else:
+    data = sys.argv[1]
+
+#initialize files for readining
+cap = cv2.VideoCapture('./data/eyetracking/' + data + '/gaze_video_processed.mov')
+rawTrackingData = open('./data/eyetracking/' + data + '/eyetracking_data_raw.txt', 'r')
+computedFrames = open('./data/eyetracking/' + data + '/computed_frames.txt', 'r')
+dataLine = json.loads(rawTrackingData.readline())
+frameLine = (computedFrames.readline() + computedFrames.readline() + computedFrames.readline()).split(';')
+
+if not os.path.exists('./out/' + data):
+    os.makedirs('./out/' + data)
+
+with open('./out/' + data + '/eyetracking.csv', mode='w') as eyetracking:
+    eyetracking_writer = csv.writer(eyetracking, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    eyetracking_writer.writerow(['Timestamp', 'x', 'y'])
+
+#read first frame
+ret, frame = cap.read()
+cv2.imshow('frame',frame)
+
+#initialize video output
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+inputWidth, inputHeight, c = frame.shape
+out = cv2.VideoWriter('./out/' + data + '/gazeVideo.avi',fourcc, 25, (inputHeight,inputWidth))
+
+#syncing: Skip first data frames that were recorded before the frist frame was processed
+dataTs = datetime.strptime(dataLine['ats'], "%Y-%m-%d-%H-%M-%S-%f")
+frameTs = datetime.strptime(frameLine[1], " %Y-%m-%d-%H-%M-%S-%f")
+while frameTs > dataTs:
+    dataLine = rawTrackingData.readline()
+    dataLine = json.loads(dataLine)
+    dataTs = datetime.strptime(dataLine['ats'], "%Y-%m-%d-%H-%M-%S-%f")
+
+#initialize first transformation matrix
+frameMtx = frameLine[6]
+for char in ["[","]","\n","\r"]:
+    frameMtx = frameMtx.replace(char,"")
+
+frameMtx = np.asarray(frameMtx.replace("  ", " ").split(" ")[1:], dtype=np.float32)
+frameMtx.shape = (3,3)
+
+#loop through all frames
+while cap.isOpened():
+
+    dataLine = rawTrackingData.readline()
+    dataLine = json.loads(dataLine)
+
+    #print str(frameTs) + " < " + str(dataTs)
+    if 'gp' in dataLine:
+        dataTs = datetime.strptime(dataLine['ats'], "%Y-%m-%d-%H-%M-%S-%f")
+        frameTs = datetime.strptime(frameLine[1], " %Y-%m-%d-%H-%M-%S-%f")
+
+        #there are more data entries than processed frames -> sync their timestamps
+        if dataTs > frameTs:
+
+            #preparing mtx (str to np.array)
+            frameMtx = frameLine[6]
+            for char in ["[","]","\n","\r"]:
+                frameMtx = frameMtx.replace(char,"")
+
+            frameMtx = np.asarray(frameMtx.replace("  ", " ").split(" ")[1:], dtype=np.float32)
+            frameMtx.shape = (3,3)
+
+            ret, frame = cap.read()
+
+            #break condition
+            if not ret:
+                break;
+
+            frameLine = (computedFrames.readline() + computedFrames.readline() + computedFrames.readline()).split(';')
+            frameTs = datetime.strptime(frameLine[1], " %Y-%m-%d-%H-%M-%S-%f")
+
+        #transforming gazepoint
+        rawX = dataLine['gp'][0]
+        rawY = dataLine['gp'][1]
+
+        rawXpx = rawX * 960
+        rawYpx = rawY * 540
+
+        gp = np.array([[[rawXpx, rawYpx]]], dtype = "float32")
+        #gpDist = cv2.undistortPoints(gp, cameraMatrix, dist) #needed?
+        #gp[0][0][0] -=  gpDist[0][0][0]
+        #gp[0][0][1] -=  gpDist[0][0][1]
+        transformedGp = cv2.perspectiveTransform(gp, frameMtx)[0][0]
+
+        cv2.circle(frame, (transformedGp[0],transformedGp[1]), 5, (0,0,255), -1)
+
+        with open('./out/' + data + '/eyetracking.csv', mode='a') as eyetracking:
+            eyetracking_writer = csv.writer(eyetracking, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            eyetracking_writer.writerow([dataTs,transformedGp[0],transformedGp[1]])
+
+        # Display the resulting frame
+        out.write(frame)
+        #cv2.imshow('frame',frame)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #    break
+
+# When everything done, release the capture
+out.release()
+cap.release()
+cv2.destroyAllWindows()

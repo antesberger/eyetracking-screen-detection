062f984078b8da41e0a4078f26c3ae5f1a5e8d97
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2019-02-12 09:53:47 +0100

    fixed error copying phone data on windows

--- scripts/copy phone data.bat
+++ scripts/copy phone data.bat
@@ -1,6 +1,11 @@
 @echo off
 adb shell "mkdir /sdcard/phoneData"
 
+adb shell run-as gazetracking.lmu.com.eyetracking_map_webview "rm -r files/.Fabric;"
+adb shell run-as com.google.firebase.codelab.friendlychat "rm -r files/.Fabric;"
+adb shell run-as com.lmu.gazetracking.eyetrackinggallery "rm -r files/.Fabric;"
+adb shell run-as gazetracking.lmu.com.eyetacking_accuracy_testing "rm -r files/.Fabric;"
+
 adb shell run-as gazetracking.lmu.com.eyetracking_map_webview "cp -r ./files /sdcard/phoneData/map"
 adb shell run-as com.google.firebase.codelab.friendlychat "cp -r ./files /sdcard/phoneData/chat"
 adb shell run-as com.lmu.gazetracking.eyetrackinggallery "cp -r ./files /sdcard/phoneData/gallery"

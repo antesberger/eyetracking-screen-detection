d2fda263bcf4f44b7c3a28c1d707f9ee9198b46a
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2019-02-19 10:06:53 +0100

    added some comments

--- postprocessing/gazeHeatmap.py
+++ postprocessing/gazeHeatmap.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 start = -1
 end = -1
 
-if len(sys.argv) < 2:
-    print "Please provide arguments in the form: <directory> <start time> <end time>"
-    print "The time is to be given in seconds"
-    print "If start and end argument are left free, the whole data will be considered"
+#read arguments
+if len(sys.argv) < 2 or len(sys.argv) > 4:
+    print("Please provide arguments in the form: <directory> <start time> <end time>")
+    print("Start and end time (both given in seconds) are optional")
     sys.exit(0)
 elif len(sys.argv) == 2:
     data = sys.argv[1]
@@ -24,22 +24,19 @@ elif len(sys.argv) == 4:
     start = sys.argv[2]
     end = sys.argv[3]
 
-    if int(start) > int(end):
-        print ("The start argument should be bigger than the end.")
-        sys.exit(0)
-else:
-    print "Please provide arguments in the form: <directory> <start second> <end second>"
-    print "The time is to be given in seconds"
-    print "If start and end argument are left free, the whole data will be considered"
+if int(start) > int(end):
+    print ("The start argument should be bigger than the end.")
     sys.exit(0)
 
 xCoordinates = np.array([])
 yCoordinates = np.array([])
 startTs = 0
 
+#loop through gaze coordinates
 with open('./out/' + data + '/eyetracking.csv', mode='r') as csvinput:
     csv_reader = csv.reader(csvinput, delimiter=',')
 
+    #ignore first row only containing header info
     header = True
     for row in csv_reader:
         if header == True:
@@ -60,20 +57,23 @@ with open('./out/' + data + '/eyetracking.csv', mode='r') as csvinput:
 
         currentTimePosition = gazeTs - startTs
 
+        #filter for chosen timeframe
         if start != -1 and end != -1:
             if currentTimePosition < dt.timedelta(0,int(start)) or currentTimePosition > dt.timedelta(0,int(end)):
                 continue
 
+        #only consider gaze events which take place on the screen
         if gazeX >= 0 and gazeX <= 1440 and gazeY >= 0 and gazeY <= 2880:
             xCoordinates = np.append(xCoordinates, gazeX)
             yCoordinates = np.append(yCoordinates, gazeY)
 
+#prepare and save heatmap plot
 heatmap, xedges, yedges = np.histogram2d(xCoordinates, yCoordinates, bins=(45,90))
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
 plt.clf()
 
-if start >= 0 and end >= 0:
+if int(start) >= 0 and int(end) >= 0:
     plt.title('Gaze Heatmap [' + start + 's,' + end + 's]')
 else:
     plt.title('Gaze Heatmap')

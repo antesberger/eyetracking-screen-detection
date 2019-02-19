20eca53fcf73cf49776ec47189290a058dac11de
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2019-02-19 11:46:53 +0100

    added degrees to accuracy check script

--- postprocessing/checkAccuracy.py
+++ postprocessing/checkAccuracy.py
@@ -9,11 +9,21 @@ from datetime import datetime
 import datetime as dt
 import matplotlib.pyplot as plt
 import math
+import configparser
 
 if len(sys.argv) != 2:
-    print "Please provide the folder name as an argument"
+    print("Please provide the folder name as an argument")
+    sys.exit(0)
 else:
     data = sys.argv[1]
+
+#read config params
+config = configparser.ConfigParser()
+config.read('../config.ini')
+screenHeightMM = int(config['DEFAULT']['screenHeightMM'])
+screenWidthMM = int(config['DEFAULT']['screenWidthMM'])
+screenHeightPX = int(config['DEFAULT']['screenHeightPX'])
+screenWidthPX = int(config['DEFAULT']['screenWidthPX'])
 
 # pixel postions on 1440x2880 pixel screen
 marker = [(329,843), (720, 843), (1112, 843), (343, 1505), (720, 1505), (1112, 1505), (329, 2170), (720, 2155), (1112, 2170)]
@@ -25,12 +35,15 @@ logLine = log.readline()
 logTs = datetime.strptime(logLine.split("; ")[0], "%Y-%m-%d %H:%M:%S.%f")
 logTarget = logLine.split("; ")[1][-2:-1]
 
-deviationsX = []
-deviationsY = []
+deviationsXpx = []
+deviationsYpx = []
+totalDeviationsPx = []
+totalDeviationsMm = []
+deviationsDeg = []
 
 with open('./out/' + data + '/accuracy.csv', mode='a') as csvoutput:
     csv_writer = csv.writer(csvoutput, lineterminator='\n')
-    csv_writer.writerow(['Eyetracking ts', 'Target appeared ts', 'deviation x', 'deviation y', 'mean deviation x', 'mean deviation y'])
+    csv_writer.writerow(['Eyetracking ts', 'Target appeared ts', 'deviation x in px', 'deviation y in px', 'deviation in px', 'deviation mm', 'deviation degree', 'mean deviation px', 'mean deviation mm', 'mean deviation degree'])
 
     with open('./out/' + data + '/eyetracking.csv', mode='r') as csvinput:
         csv_reader = csv.reader(csvinput, delimiter=',')
@@ -44,6 +57,7 @@ with open('./out/' + data + '/accuracy.csv', mode='a') as csvoutput:
             gazeTs = row[0]
             gazeX = int(float(row[1]))
             gazeY = int(float(row[2]))
+            gazeZ = float(row[3])
 
             try: #milliseconds are missing for a few eyetracking timestamps
                 gazeTs = datetime.strptime(gazeTs, "%Y-%m-%d %H:%M:%S.%f")
@@ -51,16 +65,39 @@ with open('./out/' + data + '/accuracy.csv', mode='a') as csvoutput:
                 gazeTs = datetime.strptime(gazeTs, "%Y-%m-%d %H:%M:%S")
 
             if gazeTs > logTs and gazeTs < (logTs + dt.timedelta(0,3)):
-                deviationX = int(math.sqrt((int(marker[int(logTarget) - 1][0]) - gazeX) ** 2))
-                deviationY = int(math.sqrt((int(marker[int(logTarget) - 1][1]) - gazeY) ** 2))
 
-                deviationsX.append(deviationX)
-                deviationsY.append(deviationY)
+                #get devation in px
+                deviationXpx = int(math.sqrt((int(marker[int(logTarget) - 1][0]) - gazeX) ** 2))
+                deviationYpx = int(math.sqrt((int(marker[int(logTarget) - 1][1]) - gazeY) ** 2))
+                totalDeviationPx = int(math.sqrt((deviationXpx ** 2) + (deviationYpx ** 2)))
 
-                meanDeviationX = reduce(lambda x, y: x + y, deviationsX) / len(deviationsX)
-                meanDeviationY = reduce(lambda x, y: x + y, deviationsY) / len(deviationsY)
+                #deviation in mm
+                deviationXmm = float(screenWidthMM) * (float(deviationXpx)/float(screenWidthPX))
+                deviationYmm = float(screenHeightMM) * (float(deviationYpx)/float(screenHeightPX))
+                totalDeviationMM = int(math.sqrt((deviationXmm ** 2) + (deviationYmm ** 2)))
 
-                csv_writer.writerow([gazeTs, logTs, deviationX, deviationY, meanDeviationX, meanDeviationY])
+                #deviation angle: tan(a) = gegenkathete/ankathete
+                if gazeZ != 0:
+                    tanA = totalDeviationMM/gazeZ
+                    deviationDeg =  math.degrees(math.atan(tanA))
+                else:
+                    deviationDeg = 0
+
+                #getting mean values
+                deviationsXpx.append(deviationXpx)
+                deviationsYpx.append(deviationYpx)
+                totalDeviationsPx.append(totalDeviationPx)
+                totalDeviationsMm.append(totalDeviationMM)
+                deviationsDeg.append(deviationDeg)
+
+                meanDeviationXpx = reduce(lambda x, y: x + y, deviationsXpx) / len(deviationsXpx)
+                meanDeviationYpx = reduce(lambda x, y: x + y, deviationsYpx) / len(deviationsYpx)
+                meanTotalDeviationPx = reduce(lambda x, y: x + y, totalDeviationsPx) / len(totalDeviationsPx)
+                meanTotalDeviationMm = reduce(lambda x, y: x + y, totalDeviationsMm) / len(totalDeviationsMm)
+                meanDeviationDeg = reduce(lambda x, y: x + y, deviationsDeg) / len(deviationsDeg)
+
+                #writing values to csv
+                csv_writer.writerow([gazeTs, logTs, deviationXpx, deviationYpx, totalDeviationPx, totalDeviationMM, deviationDeg, meanTotalDeviationPx, meanTotalDeviationMm, meanDeviationDeg])
             elif gazeTs > logTs:
                 logLine = log.readline()
                 if 'task ended' in logLine:

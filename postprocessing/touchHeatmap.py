606250d3e344fa7fc4868179a9a050aa9dd5a6d8
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2019-02-19 10:07:46 +0100

    added filter option of gesture type to touchHeatmap

--- postprocessing/touchHeatmap.py
+++ postprocessing/touchHeatmap.py
@@ -11,70 +11,82 @@ import matplotlib.pyplot as plt
 
 start = -1
 end = -1
+eventType = None
 
-if len(sys.argv) < 2:
-    print "Please provide arguments in the form: <directory> <start time> <end time>"
-    print "If start and end argument are left free, the whole data will be considered"
+#read arguments
+if len(sys.argv) < 2 or len(sys.argv) > 5:
+    print("Please provide arguments in the form: <directory> <start time> <end time> <event type>")
+    print("Start, end time (both given in seconds) and event type are optional")
+    print("Event types: Scroll, SingleTapConfirmed, DoubleTap, Down, ShowPress, SingleTapUp, LongPress and Fling")
     sys.exit(0)
 elif len(sys.argv) == 2:
     data = sys.argv[1]
 elif len(sys.argv) == 4:
     data = sys.argv[1]
-    start = sys.argv[2]
-    end = sys.argv[3]
+    start = int(sys.argv[2])
+    end = int(sys.argv[3])
+elif len(sys.argv) == 5:
+    data = sys.argv[1]
+    start = int(sys.argv[2])
+    end = int(sys.argv[3])
+    eventType = sys.argv[4]
 
-    if int(start) > int(end):
+if int(start) > int(end):
         print ("The start argument should be bigger than the end.")
         sys.exit(0)
-else:
-    print "Please provide arguments in the form: <directory> <start second> <end second>"
-    print "If start and end argument are left free, the whole data will be considered"
-    sys.exit(0)
 
 xCoordinates = np.array([])
 yCoordinates = np.array([])
 startTs = 0
 
-with open('./out/' + data + '/motionEvents.csv', mode='r') as csvinput:
+#loop through touch events
+with open('./out/' + data + '/gesture.csv', mode='r') as csvinput:
     csv_reader = csv.reader(csvinput, delimiter=',')
 
+    #first row is only header info
     header = True
     for row in csv_reader:
         if header == True:
             header = False
             continue
 
-        gazeTs = row[0]
-        gazeX = int(float(row[4]))
-        gazeY = int(float(row[5]))
+        touchTs = row[0]
+        currentEventType = row[1]
+        touchX = int(float(row[5]))
+        touchY = int(float(row[6]))
 
         try: #milliseconds are missing for a few eyetracking timestamps
-            gazeTs = datetime.strptime(gazeTs, "%Y-%m-%d %H:%M:%S.%f")
+            touchTs = datetime.strptime(touchTs, "%Y-%m-%d %H:%M:%S.%f")
         except:
-            gazeTs = datetime.strptime(gazeTs, "%Y-%m-%d %H:%M:%S")
+            touchTs = datetime.strptime(touchTs, "%Y-%m-%d %H:%M:%S")
 
+        #only consider the chosen event type
+        if eventType != None and not eventType.lower() in currentEventType.lower():
+            continue
+
+        #filter for chosen timeframe
         if startTs == 0:
-            startTs = gazeTs
+            startTs = touchTs
 
-        currentTimePosition = gazeTs - startTs
-
+        currentTimePosition = touchTs - startTs
         if start != -1 and end != -1:
             if currentTimePosition < dt.timedelta(0,int(start)) or currentTimePosition > dt.timedelta(0,int(end)):
                 continue
 
-        if gazeX >= 0 and gazeX <= 1440 and gazeY >= 0 and gazeY <= 2880:
-            xCoordinates = np.append(xCoordinates, gazeX)
-            yCoordinates = np.append(yCoordinates, gazeY)
+        if touchX >= 0 and touchX <= 1440 and touchY >= 0 and touchY <= 2880:
+            xCoordinates = np.append(xCoordinates, touchX)
+            yCoordinates = np.append(yCoordinates, touchY)
 
+#prepare and save heatmap plot
 heatmap, xedges, yedges = np.histogram2d(xCoordinates, yCoordinates, bins=(120,240))
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
 plt.clf()
 
 if start >= 0 and end >= 0:
-    plt.title('Gaze Heatmap [' + start + 's,' + end + 's]')
+    plt.title('Touch Heatmap [' + str(start) + 's,' + str(end) + 's]')
 else:
-    plt.title('Gaze Heatmap')
+    plt.title('Touch Heatmap')
 
 plt.imshow(heatmap.T, extent=extent, origin='lower', interpolation='nearest')
 plt.set_cmap('BuPu')

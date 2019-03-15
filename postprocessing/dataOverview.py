333e0210a9e62eaf182f32cfadd27bdf73d56f9d
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2019-03-15 17:09:01 +0100

    added average error rate, problem rate and accuracy

--- postprocessing/dataOverview.py
+++ postprocessing/dataOverview.py
@@ -8,21 +8,44 @@ if len(sys.argv) != 2:
 else:
     directory = sys.argv[1]
 
+fileCount = 0
+errorRates = []
+problemRates = []
+accuracies = []
+
 for filename in os.listdir(directory):
-    print(filename)
 
     currentLastRow = ''
     if filename != ".DS_Store":
         try:
             with open(directory + "/" + filename + '/out/log.csv', mode='r') as csvinput:
                 csv_reader = csv.reader(csvinput, delimiter=',')
+                fileCount += 1
 
                 for row in csv_reader:
                     currentLastRow = row
 
-                with open(directory + '/overview.csv', mode='a') as csvoutput:
-                    csv_writer = csv.writer(csvoutput, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                    csv_writer.writerow([filename, currentLastRow[0], currentLastRow[1], currentLastRow[2], currentLastRow[3], currentLastRow[4], currentLastRow[5], currentLastRow[6], currentLastRow[7]])
+                errorRates.append(currentLastRow[4])
+                problemRates.append(currentLastRow[7])
 
         except:
             pass
+
+    if 'Acc' in filename:
+        with open(directory + "/" + filename + '/out/accuracy.csv', mode='r') as csvinput:
+            csv_reader = csv.reader(csvinput, delimiter=',')
+
+            for row in csv_reader:
+                currentLastRow = row
+
+            accuracies.append(currentLastRow[9])
+
+
+avgErrorRate = reduce(lambda x, y: float(x) + float(y), errorRates) / len(errorRates)
+avgProblemRate = reduce(lambda x, y: float(x) + float(y), problemRates) / len(problemRates)
+avgAccuracy = reduce(lambda x, y: float(x) + float(y), accuracies) / len(accuracies)
+
+with open(directory + '/overview.csv', mode='a') as csvoutput:
+    csv_writer = csv.writer(csvoutput, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(['Participant Count', 'Avg error rate', 'Avg problem rate', 'Avg gaze deviation (degree)'])
+    csv_writer.writerow([fileCount/4,avgErrorRate, avgProblemRate, avgAccuracy])
